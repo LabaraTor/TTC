400c8af6cfd345efc8307964bd55db7c0aeb6ca5
Tornike <tornike99@yandex.ru>
2018-05-06 00:59:16 +0300

    Initial Commit

---
+++ TTC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TTC
+//
+//  Created by Торнике Двалашвили on 06.05.18.
+//  Copyright © 2018 Торнике Двалашвили. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
