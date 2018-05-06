ac99ca8ca12f643365e66128a45f2c590e0c3894
Tornike <tornike99@yandex.ru>
2018-05-06 13:49:29 +0300

    3

--- TTC/AuthVC.swift
+++ TTC/AuthVC.swift
@@ -15,6 +15,7 @@ class AuthVC: UIViewController {
 
     @IBOutlet weak var LogTF: SkyFloatingLabelTextField!
     @IBOutlet weak var PasTF: SkyFloatingLabelTextField!
+    var handle: AuthStateDidChangeListenerHandle!
     
     
     override func viewDidLoad() {
@@ -25,21 +26,29 @@ class AuthVC: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
-//    override func viewWillAppear(_ animated: Bool) {
-//        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
-//            let alert = UIAlertController(title: "Sign In", message: "Success", preferredStyle: UIAlertControllerStyle.alert)
-//            self.present(alert, animated: true, completion: nil)
-//        }
-//    }
-//
-//    override func viewWillDisappear(_ animated: Bool) {
-//        Auth.auth().removeStateDidChangeListener(handle!)
-//    }
+    override func viewWillAppear(_ animated: Bool) {
+        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+            let alert = UIAlertController(title: "Sign In", message: "Success", preferredStyle: UIAlertControllerStyle.alert)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        Auth.auth().removeStateDidChangeListener(handle!)
+    }
 
     @IBAction func SignIn(_ sender: Any) {
         Auth.auth().signIn(withEmail: LogTF.text!, password: PasTF.text!) { (user, error) in
             if let user = user {
                 let alert = UIAlertController(title: "Sign In", message: "\(user.uid)", preferredStyle: UIAlertControllerStyle.alert)
+                let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                alert.addAction(action)
+                self.present(alert, animated: true, completion: nil)
+            }
+            if let error = error {
+                let alert = UIAlertController(title: "Error", message: "\(error.localizedDescription)", preferredStyle: UIAlertControllerStyle.alert)
+                let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                alert.addAction(action)
                 self.present(alert, animated: true, completion: nil)
             }
         }
